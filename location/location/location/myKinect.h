6ab4a387d8323bc4a880775624cadaf73fda17d6
square123 <1006039518@qq.com>
2017-04-11 19:52:39 +0800

    kinect类声明

---
+++ location/location/location/myKinect.h
@@ -0,0 +1,89 @@
+#ifndef MYKINECT_H
+#define MYKINECT_H
+#include <opencv2/opencv.hpp>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+#include <strsafe.h>
+#include <string>
+#include <kinect.h>
+#include<fstream>
+#include<math.h>
+
+#define FilePath "H://testColor//"
+#define saveDepth 1
+#define saveColor 2
+#define PI 3.1415926
+using namespace cv;
+using namespace std;
+
+template<class Interface> 
+inline void SafeRelease( Interface *& pInterfaceToRelease )
+{
+	if ( pInterfaceToRelease != NULL )
+	{
+		pInterfaceToRelease->Release();
+		pInterfaceToRelease = NULL;
+	}
+}
+
+
+class Kinect
+{
+public:
+	static const int DepthWidth = 512;
+	static const int DepthHeight = 424;
+	static const int ColorWidth = 1920;
+	static const int ColorHeight = 1080;
+
+	char saveTmpStrDep[1000];
+	int saveTmpIntDep;
+	char saveTmpStrClr[1000];
+	int saveTmpIntClr;
+
+	Mat depthMat8;
+	Mat depthMat16;
+	Mat colorMat;
+	Mat colorHalfSizeMat;
+	Mat CoordinateMapperMat;//匹配后的矩阵
+	Mat CoordinateMapperMat_TM;//掩模后的函数，用于显示结果
+	Mat TwoMat;//二值图像矩阵
+	Mat TwoMatTemp;
+	Mat depthDeTemp;//深度图像平均去噪的矩阵
+	Mat showOrbit;
+	HRESULT hResult ;
+	HRESULT InitKinect();
+	HRESULT InitDepth();
+	HRESULT InitColor();
+	HRESULT InitCoorMap();
+	HRESULT Initbody();
+	ofstream outfile;//用于存储轨迹数据的
+	void colorProcess();
+	void depthProcess();//该方法在读取16位时，内存会出现问题,采用的是整体赋值
+	void depthProcess2();//该方法没有问题，采用的是分别赋值
+	void kincetSave(Mat a,string savePath,int opt);//有两种模式选择，保存彩色或深度
+	void kinectSaveAll(string savePath);//同时保存彩色和深度
+	void bodyLocation();
+	//旋转因子转换函数 
+	//画出区域检测函数
+	//时间输出函数
+	//需要按照bodyIndex来存储特定的轨迹,明天需要把函数过一遍
+	Kinect();
+	~Kinect();
+
+private:
+	IKinectSensor* pSensor ;
+	IColorFrameSource* pColorSource;
+	IColorFrameReader* pColorReader;
+	IFrameDescription* pColorDescription;
+	IColorFrame* pColorFrame;
+	IDepthFrameSource* pDepthSource;
+	IDepthFrameReader* pDepthReader;
+	IFrameDescription* pDepthDescription;
+	IDepthFrame* pDepthFrame;
+	IBodyFrameSource* pBodySource;
+	IBodyFrameReader* pBodyReader;
+	IBodyFrame* pBodyFrame;
+	ICoordinateMapper* pCoordinateMapper;
+};
+#endif
