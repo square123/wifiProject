79f8d079bdb2fbba12f46565d4263e0d5209b99c
square123 <1006039518@qq.com>
2017-05-26 20:22:29 +0800

    将原来的写法改成类，并提供了压缩到秒的函数，方便以后整合

--- newVibeTest/vibeTest/vibeTest/main.cpp
+++ newVibeTest/vibeTest/vibeTest/main.cpp
@@ -1,8 +1,9 @@
 #include "ViBe.h"
 #include "gamma.h"
-#include "otherfun.h"
+#include "overlapPro.h"
 #include <cstdio>
 #include "myKinect.h"
+#include "others.h"
 
 using namespace cv;
 using namespace std;
@@ -10,8 +11,6 @@ using namespace std;
 //还需要完成连通区域的连续标记部分，可能高斯模型比较慢，暂时先不考虑这个。合并也再说
 //通过彩色模型或许程序会比较慢，看看能不能通过连续的情况实现。这个是整个程序的关键，或许有现成的跟踪函数
 //这个是重头，完成了连续标记
-
-
 
 int main(int argc, char* argv[])
 {
@@ -34,7 +33,7 @@ int main(int argc, char* argv[])
 	//GMM 部分 为了程序速度暂时先不用
 	cv::BackgroundSubtractorMOG mog(5,5,0.7,10);
 	Mat foregroud,background;
-	;
+
 	//opticalflow 部分（不好用）
 	//Mat prvGray, optFlow ,absoluteFlow, img_for_show;
 
@@ -42,15 +41,14 @@ int main(int argc, char* argv[])
 	Mat element=getStructuringElement(MORPH_ELLIPSE,Size(3,3));
 	
 	//overlap算法部分
+	overlapPro olr;
+	olr.outfile.open("cameradata.csv",fstream::app);
 
-	vector<bwMix> prePicBwMix;
-	prePicBwMix.reserve(100);
 
 	while (1)//处理部分
 	{	
 		Mat frame;
-		vector<bwMix> picBwMix;
-		picBwMix.reserve(100);
+
 		////kinect部分
 		//kinect.colorProcess();
 		//resize(kinect.colorHalfSizeMat,frame,Size(),0.3,0.3);
@@ -65,7 +63,7 @@ int main(int argc, char* argv[])
 		if (frame.empty())
 			break;
 		//gammaCorrection (frame, 0.8);//只是调整了一下整体亮度，并没有很好的效果 而且加入这个速度会变得很慢
-        Mat  gray,out,outed;
+        Mat  gray,outed;
 		//medianBlur(frame,out,7);
 		cvtColor(frame, outed, CV_RGB2GRAY);
 		//equalizeHist(outed,out);
@@ -125,44 +123,7 @@ int main(int argc, char* argv[])
 		//	imshow("merge",mergeMat);
 		//}
 		
-		//从检测到120帧之后才开始查找，需要这个时间来初始化图像，不然会出现ghost现象
-		if(1)//这个应该是需要根据实际情况能修改的count>=20
-		{
-			//count--;
-			//这里才开始进行函数处理，不然连通区域太多了
-			Mat selectedMat;
-			selectArea(maskeded,selectedMat,180,4000);//通过函数对一些连通域小的进行筛选  以后可以根据实地判断筛选 针对稀疏人物目标的跟踪
-			imshow("dd",selectedMat);
-			//还应该再进行大小的判断 项目用 否则会出很大的问题，还有长宽的判断
-	//算法后续步骤：
-			//将连通区域用矩形表示，将区域用矩形填充
-			Mat hullMat;
-			hullArea(selectedMat,hullMat);
-			imshow("hh",hullMat);
-			//再将连通区域合并 防止出人像有缺失  （暂时不考虑，默认性能比较好）
-			//获取结构体（定义函数结构体）
-			getBwMix(hullMat,picBwMix);//得到结构体
-
-			//对结构体进行更新
-			//下幅图像标记引用上幅图像的标记 （输入是前一帧和后一帧）
-			//如果没有标记则重新赋值一个新的标记 
-			//当标记已经不存在时，要想办法去重（怎么处理丢失问题，这个也很重要，现在先不考虑，等5月结束后，写完论文再考虑该事情，加大算法的复杂度）
-			if (prePicBwMix.size()!=0)
-			{
-				renewVec(prePicBwMix,picBwMix,0.0);
-			}
-			prePicBwMix=picBwMix;//对数据进行更新
-
-			//彩色显示	//用其他颜色显示（提前给函数赋值好颜色）
-			Mat showColor=Mat::zeros(hullMat.size(),CV_8UC3);
-			bwMixToColorMat(picBwMix,showColor);
-			imshow("color",showColor);
-			//输出轨迹
-			bwMixToOrbit(picBwMix);
-
-			
-
-		}
+		olr.process(maskeded);//进行连通区域的overlap跟踪
 
 		if ( cvWaitKey(50) == 'q' )
 		{
---
+++ newVibeTest/vibeTest/vibeTest/others.h
@@ -0,0 +1,146 @@
+#pragma once
+#include <iostream>
+#include "opencv2/opencv.hpp"
+#include <vector>
+#include <set>
+#include <time.h>
+#include "vibe.h"
+
+using namespace cv;
+
+void fillHole(const Mat srcBw, Mat &dstBw)//参考网上的填充函数，很巧妙
+{
+	Size m_Size = srcBw.size();
+	Mat Temp=Mat::zeros(m_Size.height+2,m_Size.width+2,srcBw.type());//延展图像
+	srcBw.copyTo(Temp(Range(1, m_Size.height + 1), Range(1, m_Size.width + 1)));
+
+	cv::floodFill(Temp, Point(0, 0), Scalar(255));//非常巧妙，用了填充算法的反运算
+
+	Mat cutImg;//裁剪延展的图像
+	Temp(Range(1, m_Size.height + 1), Range(1, m_Size.width + 1)).copyTo(cutImg);
+
+	dstBw = srcBw | (~cutImg);
+}
+
+void  bwLabel( const Mat &bwImg, Mat &labImg )  //参考网上的连通区域标记函数
+{  
+	assert( bwImg.type()==CV_8UC1 );  
+	labImg.create( bwImg.size(), CV_32SC1 ); 
+	labImg = Scalar(0);  
+	labImg.setTo( Scalar(1), bwImg );  
+	assert( labImg.isContinuous() );  
+	const int Rows = bwImg.rows - 1, Cols = bwImg.cols - 1;  
+	int label = 1;   
+	vector<int> labelSet;  
+	labelSet.push_back(0);    
+	labelSet.push_back(1);    
+	// the first pass  
+	int *data_prev = (int*)labImg.data; // 0-th row : int* data_prev = labImg.ptr<int>(i-1);  
+	int *data_cur = (int*)( labImg.data + labImg.step ); // 1-st row : int* data_cur = labImg.ptr<int>(i);  
+	for( int i=1; i<Rows; i++ ){  
+		data_cur++;  
+		data_prev++;  
+		for( int j=1; j<Cols; j++, data_cur++, data_prev++ ){  
+			if( *data_cur!=1 )  
+				continue;  
+			int left = *(data_cur-1);  
+			int up = *data_prev;  
+			int neighborLabels[2];  
+			int cnt = 0;  
+			if( left>1 )  
+				neighborLabels[cnt++] = left;  
+			if( up>1 )  
+				neighborLabels[cnt++] = up;  
+			if( !cnt ){  
+				labelSet.push_back( ++label );  
+				labelSet[label] = label;  
+				*data_cur = label;  
+				continue;  
+			}  
+			int smallestLabel = neighborLabels[0];  
+			if( cnt==2 && neighborLabels[1]<smallestLabel )  
+				smallestLabel = neighborLabels[1];  
+			*data_cur = smallestLabel;  
+			// 保存最小等价表  
+			for( int k=0; k<cnt; k++ ){  
+				int tempLabel = neighborLabels[k];  
+				int& oldSmallestLabel = labelSet[tempLabel];  
+				if( oldSmallestLabel > smallestLabel ){                            
+					labelSet[oldSmallestLabel] = smallestLabel;  
+					oldSmallestLabel = smallestLabel;  
+				}                         
+				else if( oldSmallestLabel<smallestLabel )  
+					labelSet[smallestLabel] = oldSmallestLabel;  
+			}  
+		}  
+		data_cur++;  
+		data_prev++;  
+	}  
+	// 更新等价对列表,将最小标号给重复区域  
+	for( size_t i = 2; i < labelSet.size(); i++ ){  
+		int curLabel = labelSet[i];  
+		int preLabel = labelSet[curLabel];  
+		while( preLabel!=curLabel ){  
+			curLabel = preLabel;  
+			preLabel = labelSet[preLabel];  
+		}  
+		labelSet[i] = curLabel;  
+	}  
+	// second pass  
+	data_cur = (int*)labImg.data;  
+	for( int i=0; i<Rows; i++ ){  
+		for( int j=0; j<bwImg.cols-1; j++, data_cur++ )  
+			*data_cur = labelSet[ *data_cur ];    
+		data_cur++;  
+	}  
+}
+
+void bwLabelNext(Mat &src)//作为上述连通区域标记的后续处理
+{
+	int a[500]={0};//设置连通区域的个数最多为500个
+	for(int i=0;i<src.rows;i++)
+	{
+		int* data= src.ptr<int>(i);
+		for(int j=0;j<src.cols;j++)
+		{
+			if(data[j]!=0){//寻找元素的种类
+				for(int k=0;k<500;k++)
+				{
+					if (data[j]==a[k])
+					{
+						data[j]=k+1;//这里简化运算直接重新赋值，一次性循环处理完成
+						break;
+					}
+					else if(a[k]==0)
+					{
+						a[k]=data[j];
+						data[j]=k+1;
+						break; 
+					}
+				}
+			}
+		}
+	}
+}
+
+//光流法用  不用的程序因为效果不好
+void compute_absolute_mat(const Mat& in, Mat & out)  
+{  
+	if (out.empty()){  
+		out.create(in.size(), CV_32FC1);  
+	}  
+	const Mat_<Vec2f> _in = in;  
+	for (int i = 0; i < in.rows; ++i){  
+		float *data = out.ptr<float>(i);  
+		for (int j = 0; j < in.cols; ++j){  
+			double s = _in(i, j)[0] * _in(i, j)[0] + _in(i, j)[1] * _in(i, j)[1];  
+			if (s>1){  
+				data[j] = std::sqrt(s);  
+			}  
+			else{  
+				data[j] = 0.0;  
+			}  
+
+		}  
+	}  
+}  
